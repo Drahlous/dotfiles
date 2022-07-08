c974835e36ff5bcdf3e58ec85712ec1c3528ad04
Drahlous <MattTStachowiak@gmail.com>
2022-07-07 19:10:13 -0600

    Remove node from installation script

--- install/install-packages.sh
+++ install/install-packages.sh
@@ -11,8 +11,8 @@ set -u
 # START
 #==============
 printf "Begin Installing Packages...\n"
-sudo apt-get update &>/dev/null
-sudo apt-get install stow &>/dev/null
+sudo apt update &>/dev/null
+sudo apt install stow &>/dev/null
 
 touch "$HOME/install_progress_log.txt"
 LOGFILE="$HOME/install_progress_log.txt"
@@ -22,43 +22,31 @@ printf "Installing General Dependencies...\n"
 #==============
 # General Dependencies
 #==============
-sudo apt-get install -y ca-certificates &> "$LOGFILE"
-sudo apt-get install -y gnupg &> "$LOGFILE"
-sudo apt-get install -y lsb-release &> "$LOGFILE"
-sudo apt-get install -y curl &> "$LOGFILE"
-sudo apt-get install -y lsb-release &> "$LOGFILE"
-sudo apt-get install -y lua5.3 &> "$LOGFILE"
-sudo apt-get install -y snapd &> "$LOGFILE"
-sudo apt-get install -y cmake &> "$LOGFILE"
-
-printf "Installing Node...\n"
-# Node
-sudo apt install -y nodejs npm &> "$LOGFILE"
-sudo npm install -y -g n &> "$LOGFILE"
-sudo n latest &> "$LOGFILE"
+sudo apt install -y ca-certificates &> "$LOGFILE"
+sudo apt install -y gnupg &> "$LOGFILE"
+sudo apt install -y lsb-release &> "$LOGFILE"
+sudo apt install -y curl &> "$LOGFILE"
+sudo apt install -y lsb-release &> "$LOGFILE"
+sudo apt install -y lua5.3 &> "$LOGFILE"
+sudo apt install -y snapd &> "$LOGFILE"
+sudo apt install -y cmake &> "$LOGFILE"
 
 printf "Installing C++ Essentials...\n"
 #==============
 # C++ essentials
 #==============
-sudo apt-get install -y build-essential &> "$LOGFILE"
-sudo apt-get install -y gcc &> "$LOGFILE"
-sudo apt-get install -y g++ &> "$LOGFILE"
-sudo apt-get install -y gdb &> "$LOGFILE"
-sudo apt-get install -y clang &> "$LOGFILE"
-
-printf "Installing Rust...\n"
-#==============
-# Rust
-#==============
-sudo apt-get install -y cargo &> "$LOGFILE"
+sudo apt install -y build-essential &> "$LOGFILE"
+sudo apt install -y gcc &> "$LOGFILE"
+sudo apt install -y g++ &> "$LOGFILE"
+sudo apt install -y gdb &> "$LOGFILE"
+sudo apt install -y clang &> "$LOGFILE"
 
 printf "Installing Python3...\n"
 #==============
 # Python
 #==============
-sudo apt-get install -y python3 &> "$LOGFILE"
-sudo apt-get install -y python3-pip &> "$LOGFILE"
+sudo apt install -y python3 &> "$LOGFILE"
+sudo apt install -y python3-pip &> "$LOGFILE"
 
 #==============
 # Shell Environment
@@ -66,8 +54,8 @@ sudo apt-get install -y python3-pip &> "$LOGFILE"
 
 printf "Installing ZSH...\n"
 # Zsh
-sudo apt-get install -y zsh &> "$LOGFILE"
-sudo apt-get install -y zsh-syntax-highlighting &> "$LOGFILE"
+sudo apt install -y zsh &> "$LOGFILE"
+sudo apt install -y zsh-syntax-highlighting &> "$LOGFILE"
 
 # oh-my-zsh
 if [[ ! -d "$ZSH" ]]; then
@@ -77,11 +65,12 @@ fi
 
 printf "Installing Fonts...\n"
 # Nerd Fonts
-sudo apt-get install -y fonts-powerline &> "$LOGFILE"
-sudo apt-get install -y fonts-hack-ttf &> "$LOGFILE"
+sudo apt install -y fonts-powerline &> "$LOGFILE"
+sudo apt install -y fonts-hack-ttf &> "$LOGFILE"
 mkfontscale &> "$LOGFILE"
 mkfontdir &> "$LOGFILE"
 fc-cache -f -v &> "$LOGFILE"
+rm fonts.dir && rm fonts.scale
 
 #==============
 # CLI Tools
@@ -90,31 +79,21 @@ fc-cache -f -v &> "$LOGFILE"
 printf "Installing CLI Tools...\n"
 
 # Terminal Multiplexer
-sudo apt-get install -y tmux &> "$LOGFILE"
-
-# Silver Searcher (grep replacement)
-sudo apt-get install -y silversearcher-ag &> "$LOGFILE"
+sudo apt install -y tmux &> "$LOGFILE"
 
 # Ripgrep (grep replacement)
-sudo apt-get install -y ripgrep &> "$LOGFILE"
-
-# git-completion and git-prompt
-cd || exit
-curl -OL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash &> "$LOGFILE"
-mv ~/git-completion.bash ~/.git-completion.bash &> "$LOGFILE"
-curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh &> "$LOGFILE"
-printf "git-completion and git-prompt Installed and Configured\n" &> "$LOGFILE"
+sudo apt install -y ripgrep &> "$LOGFILE"
 
 # Fuzzy Finder
-sudo apt-get install -y fzf &> "$LOGFILE"
+sudo apt install -y fzf &> "$LOGFILE"
 
 #==============
 # Editors & File Viewers
 #==============
 
 # Vim Plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &> "$LOGFILE"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Neovim
 printf "Installing Neovim...\n"
@@ -126,27 +105,17 @@ nvim --headless +PlugInstall +qall
 sudo apt install -y xsel &> "$LOGFILE"
 pip install pynvim
 
-# Pyright Language Server
-sudo npm i -g pyright &> "$LOGFILE"
-
-# Bash Language Server
-sudo npm i -g bash-language-server &> "$LOGFILE"
-
 # Clangd
-sudo apt-get install -y clangd-12 &> "$LOGFILE"
+sudo apt install -y clangd-12 &> "$LOGFILE"
 sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100 &> "$LOGFILE"
 
-# Rust Analyzer
-curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > "$LOCAL_BIN/rust-analyzer" &> "$LOGFILE"
-chmod +x "$LOCAL_BIN/rust-analyzer" &> "$LOGFILE"
-
 # Ctags
-sudo apt-get install -y exuberant-ctags &> "$LOGFILE"
+sudo apt install -y exuberant-ctags &> "$LOGFILE"
 
 printf "Installing Utils...\n"
 
 # Shellcheck
-sudo apt-get install -y shellcheck &> "$LOGFILE"
+sudo apt install -y shellcheck &> "$LOGFILE"
 
 #=============
 # Configuration Files
@@ -154,10 +123,6 @@ sudo apt-get install -y shellcheck &> "$LOGFILE"
 
 # bash
 ln -sTfv "$DOTFILES_DIR/bashrc" "$HOME/.bashrc"
-
-# kitty
-sudo apt-get install -y kitty &> "$LOGFILE"
-ln -sTfv "$DOTFILES_DIR/config/kitty" "$XDG_CONFIG_HOME/kitty"
 
 # VSCode
 mkdir -p "$XDG_CONFIG_HOME"/Code/User/
@@ -174,7 +139,7 @@ ln -sTfv "$DOTFILES_DIR/scripts/fuz.sh" "$LOCAL_BIN/fuz"
 # Cleanup and provide a summary of what has been installed
 #==============
 printf "\n====== Cleaning up ======\n"
-sudo apt-get -y autoremove &> "$LOGFILE"
+sudo apt -y autoremove &> "$LOGFILE"
 printf "\nFinished Installing Packages!\n\n"
 rm "$LOGFILE"
 
