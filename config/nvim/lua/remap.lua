f7889268fc32087d472ab49fafda999dbde7db3e
Drahlous <MattTStachowiak@gmail.com>
2023-02-03 15:00:05 -0700

    Update lsp_references command

--- config/nvim/lua/remap.lua
+++ config/nvim/lua/remap.lua
@@ -15,7 +15,7 @@ vim.keymap.set('n', '<leader>ft', '<cmd>Telescope tags<cr>')
 vim.keymap.set('n', '<leader>fm', '<cmd>Telescope marks<cr>')
 
 -- LSP
-vim.keymap.set('n', '<leader>fr', '<cmd>Telescope lsp-references<cr>')
+vim.keymap.set('n', '<leader>fr', '<cmd>Telescope lsp_references<cr>')
 local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
