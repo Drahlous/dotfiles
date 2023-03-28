a10a9c3a59cae7d8e4f85ca3e2024084cdee5c82
Drahlous <MattTStachowiak@gmail.com>
2023-03-28 14:37:47 -0600

    Add python and markdown to treesitter

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim" },
+  ensure_installed = { "c", "lua", "vim", "python", "markdown", "markdown_inline" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
