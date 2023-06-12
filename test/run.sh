65e6d404393724d290106b335421f57e8657b1bc
Drahlous <MattTStachowiak@gmail.com>
2023-06-12 15:44:26 -0600

    Allow FUSE mounting in test container

--- test/run.sh
+++ test/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --cap-add SYS_ADMIN --device /dev/fuse -it dot-docker
+docker run --rm --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined -it dot-docker
