028e8d7e25cc5e1b1ab3280761e67b4597de5a29
Drahlous <MattTStachowiak@gmail.com>
2023-06-27 09:06:38 -0600

    Remove unused null_ls sources

--- config/nvim/lua/null-ls-config.lua
+++ config/nvim/lua/null-ls-config.lua
@@ -2,8 +2,6 @@ local null_ls = require("null-ls")
 
 local sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.completion.spell,
     -- Python Styling
     null_ls.builtins.diagnostics.pydocstyle,
     null_ls.builtins.diagnostics.pylint,
