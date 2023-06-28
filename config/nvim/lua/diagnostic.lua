36b1a36f174a5eb84848d40c883fbbb762f9e4c2
Drahlous <MattTStachowiak@gmail.com>
2023-06-27 19:36:31 -0600

    Disable virtual text for diagnostics

---
+++ config/nvim/lua/diagnostic.lua
@@ -0,0 +1,7 @@
+-- Configuration for vim diagnostic options
+
+-- Disable virtual text and underlining, they're visually noisy
+vim.diagnostic.config({
+    underline = false,
+    virtual_text = false,
+})
--- config/nvim/lua/null-ls-config.lua
+++ config/nvim/lua/null-ls-config.lua
@@ -3,8 +3,12 @@ local null_ls = require("null-ls")
 local sources = {
     null_ls.builtins.formatting.stylua,
     -- Python Styling
-    null_ls.builtins.diagnostics.pydocstyle,
-    null_ls.builtins.diagnostics.pylint,
+    null_ls.builtins.diagnostics.pydocstyle.with({
+        method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+    }),
+    null_ls.builtins.diagnostics.pylint.with({
+        method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+    }),
     null_ls.builtins.formatting.black.with({
         extra_args = {
             "--line-length", "150",
