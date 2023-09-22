b1f27ae26b8a06a7b1edfd24f80a54ac2cc22811
Drahlous <MattTStachowiak@gmail.com>
2023-09-22 16:44:22 -0600

    Disable treesitter indenting

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -25,7 +25,7 @@ return {
                 additional_vim_regex_highlighting = false,
             },
             indent = {
-                enable = true,
+                enable = false,
                 disable = { 'python' }
             },
         },
