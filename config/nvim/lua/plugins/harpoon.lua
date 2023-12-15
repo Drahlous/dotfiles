e02e423980aa281b18353f2ac6272bdf9865b80e
Drahlous <MattTStachowiak@gmail.com>
2023-12-14 22:06:05 -0700

    Add harpoon2 nvim plugin

---
+++ config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,16 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    config = function()
+        local harpoon = require("harpoon")
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+    end,
+}
