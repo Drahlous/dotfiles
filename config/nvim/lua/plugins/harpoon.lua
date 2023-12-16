3df7d16fa930febcc2a12dbab52c378b6c5c2cf2
Drahlous <MattTStachowiak@gmail.com>
2023-12-15 20:31:05 -0700

    Add required setup() to harpoon config

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -5,6 +5,9 @@ return {
     opts = {},
     config = function()
         local harpoon = require("harpoon")
+
+        harpoon:setup()
+
         vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
